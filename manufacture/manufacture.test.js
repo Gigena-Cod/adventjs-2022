73745a2bab0a681a6ede40a21b4495f5e341610b
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-05-26 02:39:36 -0300

    Second

--- manufacture/manufacture.test.js
+++ manufacture/manufacture.test.js
@@ -1,27 +1,56 @@
-
-// const gifts = ['tren', 'oso', 'pelota']
-// const materials = 'tronesa'
-
-// manufacture(gifts, materials) // ["tren", "oso"]
-// 'tren' SÍ porque sus letras están en 'tronesa'
-// 'oso' SÍ porque sus letras están en 'tronesa'
-// 'pelota' NO porque sus letras NO están en 'tronesa'
-
-// const gifts = ['juego', 'puzzle']
-// const materials = 'jlepuz'
-
-// manufacture(gifts, materials) // ["puzzle"]
-
-// const gifts = ['libro', 'ps5']
-// const materials = 'psli'
-
-// manufacture(gifts, materials) // []
-
 import { describe, it, expect } from "vitest";
 import { manufacture } from "./manufacture";
 
 describe("manufacture", () => {
   it("should be a function", () => {
     expect(typeof manufacture).toBe("function");
+  });
+
+  it("should gifts params be an array", () => {
+    const gifts = "tren";
+    const materials = [];
+
+    const manufactureProps = {
+      gifts,
+      materials,
+    };
+    expect(() => manufacture(manufactureProps)).toThrow(
+      "gifts must be an array"
+    );
+  });
+
+  it("should materials params be a string", () => {
+    const gifts = [];
+    const materials = 1;
+
+    const manufactureProps = {
+      gifts,
+      materials,
+    };
+    expect(() => manufacture(manufactureProps)).toThrow(
+      "materials must be a string"
+    );
+  });
+
+  it("should return ['tren', 'oso']", () => {
+    const gifts = ["tren", "oso", "pelota"];
+    const materials = "tronesa";
+
+    const manufactureProps = {
+      gifts,
+      materials,
+    };
+    expect(manufacture(manufactureProps)).toStrictEqual(["tren", "oso"]);
+  });
+
+  it("should return []", () => {
+    const gifts = ["tren", "oso", "pelota"];
+    const materials = "";
+
+    const manufactureProps = {
+      gifts,
+      materials,
+    };
+    expect(manufacture(manufactureProps)).toStrictEqual([]);
   });
 });
